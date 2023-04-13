b8cc39a7b621b611bd0c3c44c48ce05f5d3eeafe
Maxim Sytnyk <iaroki@protonmail.com>
2023-04-13 14:31:40 +0300

    update firefox configuration

--- modules/firefox.nix
+++ modules/firefox.nix
@@ -3,18 +3,17 @@
 {
   programs.firefox = {
     enable = true;
+    profiles.firenix = {
+      id = 0;
+      isDefault = true;
     extensions = with config.nur.repos.rycee.firefox-addons; [
       ublock-origin
       darkreader
       h264ify
-      multi-account-containers
       tree-style-tab
       vimium
       auto-tab-discard
     ];
-    profiles.firenix = {
-      id = 0;
-      isDefault = true;
       settings = {
           "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
       };
@@ -36,6 +35,69 @@
     profiles.ozone = {
       id = 1;
       isDefault = false;
+    extensions = with config.nur.repos.rycee.firefox-addons; [
+      ublock-origin
+      darkreader
+      multi-account-containers
+      tree-style-tab
+      vimium
+      auto-tab-discard
+    ];
+      settings = {
+          "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+      };
+      userChrome = ''
+        /* hides the native tabs */
+        #TabsToolbar {
+          visibility: collapse;
+        }
+
+        #titlebar {
+          visibility: collapse;
+        }
+
+        #sidebar-header {
+          visibility: collapse !important;
+        }
+      '';
+    };
+    profiles.flex = {
+      id = 2;
+      isDefault = false;
+    extensions = with config.nur.repos.rycee.firefox-addons; [
+      ublock-origin
+      darkreader
+      tree-style-tab
+      vimium
+      auto-tab-discard
+    ];
+      settings = {
+          "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+      };
+      userChrome = ''
+        /* hides the native tabs */
+        #TabsToolbar {
+          visibility: collapse;
+        }
+
+        #titlebar {
+          visibility: collapse;
+        }
+
+        #sidebar-header {
+          visibility: collapse !important;
+        }
+      '';
+    };
+    profiles.svitla = {
+      id = 3;
+      isDefault = false;
+    extensions = with config.nur.repos.rycee.firefox-addons; [
+      ublock-origin
+      darkreader
+      tree-style-tab
+      vimium
+    ];
       settings = {
           "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
       };
