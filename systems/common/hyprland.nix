d5339703d5b92ef76a960b64e796104ead0daad0
Maxim Sytnyk <iaroki@protonmail.com>
2023-05-29 11:57:01 +0300

    hyprland: add foot terminal

--- systems/common/hyprland.nix
+++ systems/common/hyprland.nix
@@ -5,7 +5,7 @@
   environment.systemPackages = with pkgs; [
     pciutils usbutils dunst networkmanagerapplet
     rofi-wayland waybar tofi grim slurp wl-clipboard waylock swaylock-effects
-    hyprpaper hyprpicker keyd psmisc
+    hyprpaper hyprpicker keyd psmisc foot
   ];
 
   programs.hyprland.enable = true;
