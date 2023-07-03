674eb326b0eb4a5c1ca3c53f65c988e7d1ddf141
Maxim Sytnyk <iaroki@protonmail.com>
2023-07-03 10:59:39 +0300

    nixos: drop nnn

--- systems/thinkpad/home.nix
+++ systems/thinkpad/home.nix
@@ -13,13 +13,10 @@
     ../common/home-manager/firefox.nix
     ../common/home-manager/git.nix
     ../common/home-manager/gpg.nix
-    ../common/home-manager/nnn.nix
     ../common/home-manager/note-sync.nix
-    ../common/home-manager/rofi.nix
     ../common/home-manager/services.nix
     ../common/home-manager/session.nix
     ../common/home-manager/tmux.nix
-    ../common/home-manager/xkeysnail.nix
     ../common/home-manager/zathura.nix
     ../common/home-manager/zsh.nix
     ./work.nix
--- systems/vmware/home.nix
+++ systems/vmware/home.nix
@@ -14,11 +14,9 @@
     ../common/home-manager/git.nix
     ../common/home-manager/gpg.nix
     ../common/home-manager/note-sync.nix
-    ../common/home-manager/rofi.nix
     ../common/home-manager/services.nix
     ../common/home-manager/session.nix
     ../common/home-manager/tmux.nix
-    ../common/home-manager/xkeysnail.nix
     ../common/home-manager/zathura.nix
     ../common/home-manager/zsh.nix
   ];
