0fa8a0640913800fa7223fb41e0871772a9c9793
Maxim Sytnyk <iaroki@protonmail.com>
2024-03-05 20:42:22 +0200

    thinkpad: add opengl vaapi

--- systems/thinkpad/system.nix
+++ systems/thinkpad/system.nix
@@ -48,6 +48,16 @@
   hardware.trackpoint.enable = true;
   hardware.trackpoint.emulateWheel = true;
 
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      vaapiVdpau
+      libvdpau-va-gl
+    ];
+  };
+
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
