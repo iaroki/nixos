77be82f2b71489e4ab4492a1ef1b6ef3f1472626
Maxim Sytnyk <iaroki@protonmail.com>
2024-05-06 12:53:52 +0300

    tmux: update config

--- systems/common/home-manager/tmux.nix
+++ systems/common/home-manager/tmux.nix
@@ -4,10 +4,15 @@
   programs.tmux = {
     enable = true;
     secureSocket = false;
+    baseIndex = 1;
+    clock24 = true;
     extraConfig = ''
       set -g default-terminal "tmux-256color"
       set -ag terminal-overrides ",xterm-256color:RGB"
-      set -g status-position bottom
+      set -g base-index 1
+      setw -g pane-base-index 1
+      set-option -g renumber-windows on
+      set -g status-position top
       set -g status-justify centre
       set -g status-style "bg=#000000"
       set -g window-style ""
@@ -20,7 +25,7 @@
       set -g status-right-length 25
       set -g window-status-current-style "bold"
       set -g window-status-style ""
-      set -g window-status-format " #[fg=#C8C093][#I]#W#F "
+      set -g window-status-format " #[fg=#cdd6f4][#I]#W#F "
       set -g window-status-current-format " #[fg=#cba6f7]#W#F "
       set -g window-status-separator ""
       set -g pane-active-border-style "fg=#DCD7BA"
@@ -33,7 +38,9 @@
       bind -T copy-mode-vi v send -X begin-selection
       bind-key -T copy-mode-vi y send -X copy-pipe-and-cancel "xclip -sel clip -i"
       bind P paste-buffer
-      bind-key -r f display-popup -b rounded -E "f"
+      bind-key -r f display-popup -E "f"
+      bind-key -r g display-popup -b rounded -E "lazygit"
+      bind-key -r S display-popup -b rounded -E "tmux-session"
     '';
   };
 }
